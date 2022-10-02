152953d3109cf1cc144c0d6536fe2f097a754e8f
vishal <vishal340@vishal-asus>
2022-10-02 17:00:47 +0530

    completed b_22

--- b_22/4.cpp
+++ b_22/4.cpp
@@ -1,61 +1,87 @@
 #include<bits/stdc++.h>
 using namespace std;
-
-const int hor[4]={0,1,0,-1};
-const int ver[4]={1,0,-1,0};
-const char dir[4]={'S','E','N','W'};
-void dfs(int row,int col,const int N1,const int N2,const int num,const int total,pair<bool,int>* dp,char* ret, bool& exit){
+const int hor[4]{0,2,0,-2};
+const int ver[4]{2,0,-2,0};
+void dfs(int row,int col,int N1,int N2,int total,vector<vector<pair<char,bool>>>& dp,int& num){
+	if(num==total)
+		return;
 	for(int i=0;i<4;i++){
-		if(exit)
-			return;
 		int row1=row+ver[i];
 		int col1=col+hor[i];
-		int pos=row1*N2+col1;
-		if(row1>=0 && row1<N1 && col1>=0 && col1<N2 && dp[pos].first && !dp[pos].second){
-			dp[pos].second=num;
-			ret[num-2]=dir[i];
-			if(num==total){
-				exit=1;
-				if(ret[0]=='E')ret[num-1]='N';
-				else{ret[num-1]='W';}
-				break;
+		if(row1>=0 && col1>=0 && row1<N1 && col1<N2 && dp[row1][col1].first && !dp[row1][col1].second){
+			num++;
+			dp[row1][col1].second=true;
+			dp[row1][col1+1].second=true;
+			dp[row1+1][col1].second=true;
+			dp[row1+1][col1+1].second=true;
+			switch(i){
+				case 0:
+					dp[row1-1][col1+1].first='S';
+					dp[row1][col1].first='N';
+					break;
+				case 1:
+					dp[row1][col1-1].first='E';
+					dp[row1+1][col1].first='W';
+					break;
+				case 2:
+					dp[row1+1][col1+1].first='S';
+					dp[row1+2][col1].first='N';
+					break;
+				case 3:
+					dp[row1][col1+1].first='E';
+					dp[row1+1][col1+2].first='W';
+					break;
 			}
-			dfs(row1,col1,N1,N2,num+1,total,dp,ret,exit);
+			dfs(row1,col1,N1,N2,total,dp,num);
 		}
 	}
 }
-
 void solve(){
 	int N1,N2;
 	cin>>N1>>N2;
-	N1<<=1;N2<<=1;
-	pair<bool,int>* dp=new pair<bool,int>[N1*N2]{make_pair(0,0)};
-	dp[0]={1,1};
-	int num=0;
+	vector<vector<pair<char,bool>>> ret(2*N1,vector<pair<char,bool>>(2*N2,{'\0',0}));
 	string c;
-	for(int i=0;i<(N1>>1);i++){
+	int num=0;
+	N1<<=1;
+	for(int i=0;i<N1;i+=2){
 		cin>>c;
-		for(int k=0;k<(N2>>1);k++){
-			if(c[k]=='*'){
-				num+=4;
-				auto temp=&dp[(i<<1)*N2+(k<<1)];
-				temp[0].first=1;
-				temp[1].first=1;
-				temp[N2].first=1;
-				temp[N2+1].first=1;
+		for(int j=0;j<N2;j++){
+			if(c[j]=='*'){
+				num++;
+				ret[i][2*j].first='E';
+				ret[i][2*j+1].first='S';
+				ret[i+1][2*j].first='N';
+				ret[i+1][2*j+1].first='W';
 			}
 		}
 	}
-	char* ret=new char[num];
-	bool exit=false;
-	dfs(0,0,N1,N2,2,num,&dp[0],&ret[0],exit);
-	if(!exit)
-		cout<<"IMPOSSIBLE";
+	ret[0][0].second=true;
+	ret[0][1].second=true;
+	ret[1][0].second=true;
+	ret[1][1].second=true;
+	int connect=1;
+	dfs(0,0,N1,2*N2,num,ret,connect);
+	if(connect<num)cout<<"IMPOSSIBLE";
 	else{
-		for(int i=0;i<num;i++)cout<<ret[i];
+		int n1=0,n2=0;
+		do{
+			cout<<ret[n1][n2].first;
+			switch(ret[n1][n2].first){
+				case 'E':
+					n2++;
+					break;
+				case 'S':
+					n1++;
+					break;
+				case 'N':
+					n1--;
+					break;
+				case 'W':
+					n2--;
+					break;
+			}
+		}while(n1 || n2);
 	}
-	delete []dp;
-	delete []ret;
 }
 
 int main(){
